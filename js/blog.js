0a07a28a203eaf0a656636fba550335f9ebd6110
suvo <suvodatta72@gmail.com>
2024-09-23 13:46:15 +0600

    all design done

---
+++ js/blog.js
@@ -0,0 +1,4 @@
+
+$("nav_home_button").addEventListener("click", function () {
+    window.location.replace("/index.html")
+})
--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
 
+$("nav_blog_button").addEventListener("click", function () {
+    window.location.replace("/blog.html")
+})
+
+// Modal
+$("noakhali_donate_button").addEventListener("click", function () {
+    my_modal_1.showModal()
+})
+
+$("feni_donate_button").addEventListener("click", function () {
+    my_modal_1.showModal()
+})
+
+$("quota_movement_donate_button").addEventListener("click", function () {
+    my_modal_1.showModal()
+})
